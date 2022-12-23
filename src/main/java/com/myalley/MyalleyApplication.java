d8fae2459f02a460dce6003cacdc5c80ef18b6cf
Damm06 <ghkeka111@gmail.com>
2022-12-23 12:50:34 +0900

    initial commit

---
+++ src/main/java/com/myalley/MyalleyApplication.java
@@ -0,0 +1,13 @@
+package com.myalley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyalleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyalleyApplication.class, args);
+	}
+
+}
