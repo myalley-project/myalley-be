1d9bb11ca3b307ba3b2b08b3ef6d59459f165e67
yeongjin <hanyj0104@naver.com>
2023-01-10 14:48:00 +0900

    ㅇㅇ

--- src/main/java/com/myalley/MyalleyApplication.java
+++ src/main/java/com/myalley/MyalleyApplication.java
@@ -3,10 +3,12 @@ package com.myalley;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MyalleyApplication {
 
 	public static void main(String[] args) {
