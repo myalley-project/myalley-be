66ddedbda472e79a36fa0867f95efbb10e12ae05
Damm06 <ghkeka111@gmail.com>
2023-01-12 21:49:49 +0900

    Fix: s3 관련 중복 코드 삭제

--- src/main/java/com/myalley/MyalleyApplication.java
+++ src/main/java/com/myalley/MyalleyApplication.java
@@ -8,9 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class MyalleyApplication {
 
-	static {  System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true"); }
-
-	static {System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true"); }
+	static { System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true"); }
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyalleyApplication.class, args);
