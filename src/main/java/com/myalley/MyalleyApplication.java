cf37225cc1a787063089001b91606aced161ba9d
yeongjin han <hanyj0104@naver.com>
2023-01-19 14:38:37 +0900

    ready to merge

--- src/main/java/com/myalley/MyalleyApplication.java
+++ src/main/java/com/myalley/MyalleyApplication.java
@@ -3,13 +3,10 @@ package com.myalley;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
-@SpringBootApplication
+//@EnableJpaAuditing
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class MyalleyApplication {
 
 	static {System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true"); }
