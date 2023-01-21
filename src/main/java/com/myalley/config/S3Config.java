ece4054eae8be90ff4d7988a8d3637e2682fcc64
yeongjin han <hanyj0104@naver.com>
2023-01-21 22:54:27 +0900

    변수명 수정

--- src/main/java/com/myalley/config/S3Config.java
+++ src/main/java/com/myalley/config/S3Config.java
@@ -10,10 +10,10 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Config {
-    @Value("${cloud.aws.credentials.accessKey}")
+    @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
 
-    @Value("${cloud.aws.credentials.secretKey}")
+    @Value("${cloud.aws.credentials.secret-key}")
     private String secretKey;
 
     @Value("${cloud.aws.region.static}")
