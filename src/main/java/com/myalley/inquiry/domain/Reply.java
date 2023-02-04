64cdd4d1857af6e50fb02d1b70305e67058db155
yeongjin han <hanyj0104@naver.com>
2023-02-05 04:37:04 +0900

    member notnull 추가

--- src/main/java/com/myalley/inquiry/domain/Reply.java
+++ src/main/java/com/myalley/inquiry/domain/Reply.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Entity
@@ -22,14 +23,17 @@ public class Reply {
     @Column(name="reply_id")
     private Long replyId;
 
+    @NotNull
     @ManyToOne(targetEntity= Member.class,fetch=FetchType.LAZY)
     @JoinColumn(name="member_id")
     private Member member;
 
+    @NotNull
     @OneToOne(fetch= FetchType.LAZY)
     @JoinColumn(name="inquiry_inquiry_id", referencedColumnName = "inquiry_id")
     private Inquiry inquiry;
 
+    @NotNull
     private String reply;
 
     @CreatedDate
