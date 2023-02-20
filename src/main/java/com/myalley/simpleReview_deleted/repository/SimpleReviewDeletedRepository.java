95900c420a0bf47a62a967a1dae0a6c8a802b38e
Choi-Young-Eun <cye8891@naver.com>
2023-02-20 13:03:06 +0900

    Fix : 한 줄 리뷰 삭제 시 데이터 삭제

--- src/main/java/com/myalley/simpleReview_deleted/repository/SimpleReviewDeletedRepository.java
+++
@@ -1,7 +0,0 @@
-package com.myalley.simpleReview_deleted.repository;
-
-import com.myalley.simpleReview_deleted.domain.SimpleReviewDeleted;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SimpleReviewDeletedRepository extends JpaRepository<SimpleReviewDeleted, Long> {
-}
