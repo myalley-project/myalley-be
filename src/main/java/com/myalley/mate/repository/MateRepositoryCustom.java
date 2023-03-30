613a48f3acbc4f96f6cd8fcef728b4773f083770
Damm06 <ghkeka111@gmail.com>
2023-03-30 14:45:07 +0900

    Refactor: 커스텀 repo에 전시회 상세에서 메이트 목록 조회, 마이페이지 본인글 조회 기능 추가

--- src/main/java/com/myalley/mate/repository/MateRepositoryCustom.java
+++ src/main/java/com/myalley/mate/repository/MateRepositoryCustom.java
@@ -1,9 +1,12 @@
 package com.myalley.mate.repository;
 
-import com.myalley.mate.domain.Mate;
+import com.myalley.mate.dto.response.MateMyResponse;
+import com.myalley.mate.dto.response.MateSimpleResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface MateRepositoryCustom {
-    Page<Mate> findMates(String status, String title, Pageable pageable);	// offset,limit 전달
+    Page<MateSimpleResponse> findMatesByStatusAndTitle(String status, String title, Pageable pageable);	// offset,limit 전달
+    Page<MateSimpleResponse> findMatesByExhibitionId(Long exhibitionId, Pageable pageable);
+    Page<MateMyResponse> findMatesByMember(Long memberId, Pageable pageable);
 }
